a437c2157aa1dfc20a37fde7f3b791cf4a496aec
Kevin Ballard <kevin@tellapart.com>
2013-05-22 18:28:31 -0700

    Set release version to 1.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 
 setup(
     name='commandr',
-    version='1.0dev',
+    version='1.0',
     packages=['commandr'],
     author='Kevin Ballard',
     author_email='kevin@tellapart.com',
