2c2028a982f6d12582d6139c3a73437d0ba05c15
Kevin Ballard <kevin@tellapart.com>
2013-12-18 14:28:23 -0800

    Prepare release 1.3.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 
 setup(
     name='commandr',
-    version='1.3.1',
+    version='1.3.2',
     packages=['commandr'],
     author='Kevin Ballard',
     author_email='kevin@tellapart.com',
